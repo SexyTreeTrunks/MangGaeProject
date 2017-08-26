be406cbc212fe358568a96352ddd788a8c999b61
SexyTreeTrunks <apenr1234@gmail.com>
2017-08-26 18:17:22 +0900

    coded structure of MangGaeTest

--- app/src/test/java/com/sexytreetrunks/manggaegame/MangGaeTest.java
+++ app/src/test/java/com/sexytreetrunks/manggaegame/MangGaeTest.java
@@ -1,6 +1,10 @@
 package com.sexytreetrunks.manggaegame;
 
+import com.sexytreetrunks.manggaegame.Model.Board;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.notification.RunListener;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +13,26 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class MangGaeTest {
+    private Board board;
+
+    @Before
+    public void setup() {
+        board = new Board();
+    }
+
+    //테스트 해야 할것
+    //1. dominantmanggae를 선택했는지 판단하는 여부
+    //2. levelup 잘되는지 확인
+
+    @Test //2
+    public void isCorrectVariableWhenLevelup() {
+
+
+    }//levelup 존나게 한다음에 특정 레벨에서(변수값이 바껴야하는 레벨--> 16, 32,64,, 등등) 변수값 assert
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testCheckingDominantMangGae() {
+
     }
 }
