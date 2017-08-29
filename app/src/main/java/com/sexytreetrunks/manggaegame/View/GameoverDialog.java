263c436784cb6b61178b207409ab55d64bdcd297
SexyTreeTrunks <apenr1234@gmail.com>
2017-08-30 06:11:34 +0900

    다이얼로그 디자인 수정

--- app/src/main/java/com/sexytreetrunks/manggaegame/View/GameoverDialog.java
+++ app/src/main/java/com/sexytreetrunks/manggaegame/View/GameoverDialog.java
@@ -10,27 +10,34 @@ import com.sexytreetrunks.manggaegame.Control.MangGaeActivity;
 import com.sexytreetrunks.manggaegame.R;
 
 /**
- * Created by rentdotcom on 2017. 8. 28..
+ * Created by SexyTreetrunks on 2017. 8. 28..
  */
 
 public class GameoverDialog extends Dialog implements View.OnClickListener{
-    Button button;
-    Context context;
+    Button button_yes;
+    Button button_no;
     MangGaeActivity activity;
 
     public GameoverDialog(Context context) {
         super(context);
-        this.context = context;
         activity = (MangGaeActivity) context;
         setContentView(R.layout.activity_gameover_dialog);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-        button = findViewById(R.id.btn_yes);
-        button.setOnClickListener(this);
+        button_yes = findViewById(R.id.btn_yes);
+        button_no = findViewById(R.id.btn_no);
+        button_yes.setOnClickListener(this);
+        button_no.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        activity.recreate();
-        dismiss();
+        switch (view.getId()) {
+            case R.id.btn_yes:
+                activity.recreate();
+                dismiss();
+                break;
+            case R.id.btn_no:
+                activity.finish();
+        }
     }
 }
