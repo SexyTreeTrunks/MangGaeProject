f50512f483cdac72e389d6902d7714cbe1855931
SexyTreeTrunks <apenr1234@gmail.com>
2017-09-02 17:26:30 +0900

    다양한 망개이미지 추가

--- app/src/main/java/com/sexytreetrunks/manggaegame/View/ImageAdapter.java
+++ app/src/main/java/com/sexytreetrunks/manggaegame/View/ImageAdapter.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import com.sexytreetrunks.manggaegame.Model.Board;
 import com.sexytreetrunks.manggaegame.R;
 
+import java.util.Random;
+
 /**
  * Created by sexytreetrunks on 2017. 8. 28..
  */
@@ -18,9 +20,19 @@ public class ImageAdapter extends BaseAdapter {
     Context context;
     Board board;
     int size;
+    int[] img = {R.drawable.manggae1, R.drawable.manggae2, R.drawable.manggae3, R.drawable.manggae4, R.drawable.manggae5,
+            R.drawable.manggae7, R.drawable.manggae8, R.drawable.manggae9, R.drawable.manggae10, R.drawable.manggae11,
+            R.drawable.manggae12, R.drawable.manggae13, R.drawable.manggae14, R.drawable.manggae15, R.drawable.manggae16,
+            R.drawable.manggae17, R.drawable.manggae18, R.drawable.manggae19, R.drawable.manggae20, R.drawable.manggae21,
+            R.drawable.manggae22, R.drawable.manggae23, R.drawable.manggae24, R.drawable.manggae25, R.drawable.manggae26};
 
-    public ImageAdapter(Context context, Board board) {
+    public ImageAdapter(Context context, Board board, int width) {
         this.context = context;
+        this.board = board;
+        size = width;
+    }
+
+    public void setBoard(Board board) {
         this.board = board;
     }
 
@@ -44,7 +56,7 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
         if (view == null) {
             imageView = new ImageView(context);
-            imageView.setLayoutParams(new GridView.LayoutParams(120, 120));
+            imageView.setLayoutParams(new GridView.LayoutParams(size/board.getSize(), size/board.getSize()));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(1,1,1,1);
         } else {
@@ -52,19 +64,9 @@ public class ImageAdapter extends BaseAdapter {
         }
         int row = i / board.getSize();
         int col = i % board.getSize();
-        switch (board.getValueFromMatrix(row,col)) {
-            case 1:
-                imageView.setImageResource(R.drawable.manggae1);
-                break;
-            case 2:
-                imageView.setImageResource(R.drawable.manggae2);
-                break;
-            case 3:
-                imageView.setImageResource(R.drawable.manggae3);
-                break;
-            case 4:
-                imageView.setImageResource(R.drawable.manggae4);
-        }
+
+        imageView.setImageResource(img[board.getValueFromMatrix(row,col)]-1);
+
         return imageView;
     }
 }
